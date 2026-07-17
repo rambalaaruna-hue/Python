3832f4a2928b1062c6032a3304927ab8a6a5044b
rambalaaruna-hue/pythonfsd <rambalaaruna@gmail.com>
2026-07-17 21:07:59 +0530

    Variables

---
+++ variables.py
@@ -0,0 +1,36 @@
+# a = 10
+# print(a)
+
+# a,b,c = 1,3,4
+# print(a,b,c)
+
+# c = 1,2,3
+# print(c)
+
+
+# c=v=x=22
+# print(c)
+# print(v)
+# print(x)
+
+'''
+1.alphabets
+2.Should not start with numbers,symbols
+3._start
+4.case sensitive
+'''
+# _abc = 3
+# print(_abc)
+
+# abc4 = 3
+# print(abc4)
+
+
+# abc4 = 3
+# print(id(abc4))
+
+
+python = 1
+PYTHON = 11
+print(python)
+print(PYTHON)
