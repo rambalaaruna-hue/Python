0baf09b483ea7782f07495ce2e6e155fd2f38571
rambalaaruna-hue/pythonfsd <rambalaaruna@gmail.com>
2026-07-17 21:20:45 +0530

    DataTypes

---
+++ datatypes.py
@@ -0,0 +1,41 @@
+'''
+int(1,-1)
+boolean
+float
+complex
+'''
+
+# a = 1
+# b = -1
+# print(type(a),type(b))
+
+# a=True
+# b=False
+# print(type(a),type(b))
+
+# print(True==1)
+# print(False==0)
+
+# c=4.44
+# print(type(c))
+
+# v=2+5j
+# print(type(v))
+
+
+'''
+int()
+float()
+bool()
+str()
+'''
+
+# a=2
+# print(float(a))
+# b=5.5
+# print(int(b))
+
+'''
+data lose----> explicit conversion
+no data lose---->implicit conversion
+'''
