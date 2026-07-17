cf7695412ec5eb79b0e407a0a61551e9a071de5d
rambalaaruna-hue/pythonfsd <rambalaaruna@gmail.com>
2026-07-17 21:39:40 +0530

    Conditional Statements

---
+++ conditional.py
@@ -0,0 +1,41 @@
+'''
+decision making statements
+
+if 
+else
+elif
+nested if
+'''
+
+# if False:
+#     print("This is if condition")
+# elif True:
+#     print("This is elif")
+# else:
+#     print("This is else")
+
+
+#Nested if
+# if True:
+#     print("Outer if")
+#     if True:
+#         print("Inner if")
+#     else:
+#         print("Inner else")
+# else:
+#     print("Outer else")
+
+# age=13
+# if age>18:
+#     print("You can vote")
+# elif age==18:
+#     print("You can vote now")
+# else:
+#     print("You cannot vote")
+
+
+age=18
+if age>18 or age==18:
+    print("You can vote")
+else:
+    print("You cannot vote")
