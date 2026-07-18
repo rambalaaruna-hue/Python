6d5f535d78d67e3bbc57caaf64ffa15efd53ca99
rambalaaruna-hue/pythonfsd <rambalaaruna@gmail.com>
2026-07-18 17:36:06 +0530

    Looping Statements

---
+++ looping.py
@@ -0,0 +1,34 @@
+'''
+for
+while
+nested loop
+'''
+#for - loop
+# for i in range(0,10):
+#     print(i)
+
+# for i in range(0,10,2):
+#     print(i)
+
+
+# a = [1,5,7,8,9,20,25,4]
+# for i in a:
+#     print(i)
+
+
+# a = 'Python'
+# for k in a:
+#     print(k)
+
+#while - loop
+# a=10
+# while a <= 20:
+#     print("Hii",a)
+#     a+=1
+
+
+#Nested loop
+# for i in range(0,10):
+#     for j in range(0,50):
+#         print(i+j)
+
