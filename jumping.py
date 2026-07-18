4b76627db8a98029fe3084552021d4216f952810
rambalaaruna-hue/pythonfsd <rambalaaruna@gmail.com>
2026-07-18 17:54:10 +0530

    Jumping Statements

---
+++ jumping.py
@@ -0,0 +1,33 @@
+'''
+pass
+break
+continue
+
+'''
+
+#pass
+# if True:
+#     pass
+
+#break
+# a="pythonlife"
+# for i in a:
+#      if i=='l':
+#       break
+#      print(i)
+
+
+#continue
+a="pythonlife"
+for i in a:
+     if i=='y':
+      continue
+     print(i)
+
+'''
+
+pass---Ignore
+break---Terminate
+continue---Skip(Current Iteration)
+
+'''
