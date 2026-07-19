5414eca61b2f8e6fc3f946cefc41945ba0da0de6
rambalaaruna-hue/pythonfsd <rambalaaruna@gmail.com>
2026-07-19 12:54:22 +0530

    Tuples

---
+++ tuple.py
@@ -0,0 +1,29 @@
+# T=(1,3,4,8,10)
+#print(type(T))
+# print(T[3])
+# print(T[0:4:2])
+# print(min(T))
+# print(max(T))
+# print(sum(T))
+# print(len(T))
+
+# t1=(1,2,3)
+# t2=(4,5,6)
+# print(t1+t2)
+
+
+# T=(1,3,4,8,10)
+# print(T*3)#it repeats 3 times
+# for i in T:
+#     print(i)
+
+
+#Membership operators
+T=(1,3,4,8,10)
+print(1 in T)
+print(1 not in T)
+t1=(1,2,3)
+t2=(1,2,3)
+print(t1 is t2)
+print(t1 is not t2)
+
