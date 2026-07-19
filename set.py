57349e0c8ad1ecb872c90c7c8558dec2a7b11244
rambalaaruna-hue/pythonfsd <rambalaaruna@gmail.com>
2026-07-19 17:42:34 +0530

    Set

---
+++ set.py
@@ -0,0 +1,45 @@
+# s={1,2,4,5,25,55,2,55,5,1}
+# print(type(s))
+# print(s)
+
+'''
+Methods:
+add 
+update
+pop
+remove
+
+'''
+
+#s={1,2,4,5,25,55,2,55,5,1,0,4}
+#s.add(123)
+#s.update({35,45,65})
+#s.pop()
+#s.remove(55)
+#print(s)
+
+'''
+Operations:
+union
+intersection
+difference
+issubset
+issuperset
+
+'''
+
+# set1={1,2,3,4}
+# set2={3,4,5,6}
+#print(set1.union(set2))
+#print(set1.intersection(set2))
+#print(set1.difference(set2))
+
+
+# set1={1,2,3,4,5,6,7,8}
+# set2={1,2,3,4}
+# print(set2.issubset(set1))
+# print(set1.issuperset(set2))
+# print(set2.issuperset(set1))
+
+for i in {1,2,3,4}:
+    print(i)
