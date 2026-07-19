a3c4eca90d727bd27571102520b28ffe58724d57
rambalaaruna-hue/pythonfsd <rambalaaruna@gmail.com>
2026-07-19 13:25:48 +0530

    Dictionaries

---
+++ dictionary.py
@@ -0,0 +1,34 @@
+# d={}
+# print(type(d))
+
+# d={1:'abc',20:'aruna','pythonlife':1}
+#print(d[20])
+
+'''
+
+get
+update
+keys
+values
+items
+
+'''
+
+# d={1:'abc',20:'aruna','pythonlife':1}
+# print(d.get(1))
+# print(d.keys())
+# print(d.values())
+# print(d.items())
+# d.update({11111:22222})
+# print(d)
+
+
+d={1:'abc',20:'aruna','pythonlife':1}
+# for i in d:
+#     print(i)
+
+# for i in d.values():
+#     print(i)
+
+for i,j in d.items():
+    print(i,j)
