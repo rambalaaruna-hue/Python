a6e94cb1b8e445cf7ba0842c90e65e51340870d5
rambalaaruna-hue/pythonfsd <rambalaaruna@gmail.com>
2026-07-19 18:11:39 +0530

    Functions

---
+++ demo.py
@@ -0,0 +1,3 @@
+import functions
+functions.add(3,3)
+functions.sub(10,5)
---
+++ functions.py
@@ -0,0 +1,30 @@
+'''
+Function is a block of code that performs a specific task
+
+'''
+# def func(a,b,c):#function definition
+#     print("This is function",a,b,c)#function body
+# func(1,2,3)#function call 
+
+# def func(*a):#function definition
+#     print("This is function",a)#function body
+# func(1,2,3)#function call 
+
+
+# def func(**a):#function definition
+#     print("This is function",a)#function body
+# func(b=1,c=2,d=3)#function call 
+
+
+#Nested Function
+# def outer():
+#     print("Outer Function")
+#     def inner():
+#         print("Inner Function")
+#     inner()
+# outer()
+
+def add(a,b):
+    print(a+b)
+def sub(a,b):
+    print(a-b)
