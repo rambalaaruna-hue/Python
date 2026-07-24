b6c217f88d679a4312d3d80ab7eebe5f032e0d1d
rambalaaruna-hue/pythonfsd <rambalaaruna@gmail.com>
2026-07-24 11:10:52 +0530

    Class

---
+++ class_Topic.py
@@ -0,0 +1,20 @@
+'''
+Class is the blue print of an object
+
+'''
+# class Python():
+#     a=3
+#     print(a)
+
+
+class Python:
+    a=10
+    def output(self):
+        print(self.a)
+b=Python()
+b.output()
+v=Python()
+v.output()
+print(b.a)  
+
+ 
